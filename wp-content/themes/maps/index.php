ae4d251663f7aea5531740d5d6b1bde61c135f8a
Andrew Kostka <akostka@gunnjerkens.com>
2013-02-12 17:55:54 -0800

    Changed theme to include categories

--- wp-content/themes/maps/index.php
+++ wp-content/themes/maps/index.php
@@ -6,6 +6,17 @@
 
   ?>
 
-<div id="map" style="height:1000px;width:1000px;"></div>
+  <div id="map" style="height:500px;width:100%;"></div>
+  	<a href="#" class="map-category">View All</a>
+    <a href="#" class="map-category" data-target="Tenant Improvement">Tenant Improvement</a>
+	<a href="#" class="map-category" data-target="Industrial">Industrial</a>
+    <a href="#" class="map-category" data-target="Financial">Financial</a>
+    <a href="#" class="map-category" data-target="Ground Up">Ground Up</a>
+    <a href="#" class="map-category" data-target="Retail">Retail</a>
+    <a href="#" class="map-category" data-target="Restaurant">Restaurant</a>
+    <a href="#" class="map-category" data-target="School">School</a>
+    <a href="#" class="map-category" data-target="Government">Government</a>
+    <a href="#" class="map-category" data-target="Light Medical">Light Medical</a>
+    <a href="#" class="map-category" data-target="Fa">Fine Arts</a>
 
 <?php get_footer(); ?>
