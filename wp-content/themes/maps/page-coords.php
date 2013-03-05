cd602facf995d19c1ec8bbd8b65a0a7d4faa469f
Andrew Kostka <akostka@gunnjerkens.com>
2013-03-04 16:35:52 -0800

    updated with gj-maps class

---
+++ wp-content/themes/maps/page-coords.php
@@ -0,0 +1,764 @@
+<?php
+
+get_header();
+
+$poi = array(
+	array( 
+		"title" => "Dougherty Valley Performing Arts Center",
+		"address" => "10550 Albion Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-973-3343",
+		"url" => "http =>//www.sanramon.ca.gov/parks/theater/default.htm",
+		"cat_slug" => "arts_entertainment",
+		"desc" => "Arts & Entertainment",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "The Golden Skate",
+		"address" => "2701 Hooper Dr",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-820-2525",
+		"url" => "thegoldenskate.com_",
+		"cat_slug" => "arts_entertainment",
+		"desc" => "Arts & Entertainment",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Crow Canyon Cinemas",
+		"address" => "2525 San Ramon Valley Blvd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-820-4831",
+		"url" => "http =>//www.regmovies.com",
+		"cat_slug" => "arts_entertainment",
+		"desc" => "Arts & Entertainment",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Blackhawk Auto Museum",
+		"address" => "3700 Blackhawk Plaza Cir",
+		"city" => "Danville",
+		"zip" => "94506",
+		"phone" => "925-736-2277",
+		"url" => "http =>//www.blackhawkmuseum.org",
+		"cat_slug" => "arts_entertainment",
+		"desc" => "Arts & Entertainment",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Bay Area Crown Billiards",
+		"address" => "2416 San Ramon Valley Blvd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-725-3900",
+		"url" => "http =>//bayareacrownbilliards.com/",
+		"cat_slug" => "arts_entertainment",
+		"desc" => "Arts & Entertainment",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Century Theatres",
+		"address" => "Black Hawk Plaza",
+		"city" => "Danville",
+		"zip" => "94506",
+		"phone" => "925-736-0367",
+		"url" => "http =>//www.cinemark.com/404.aspx",
+		"cat_slug" => "arts_entertainment",
+		"desc" => "Arts & Entertainment",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Quail Run Elementary School",
+		"address" => "4000 Goldenbay Ave",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-560-4000",
+		"url" => "http =>//www.qres.srvusd.k12.ca.us",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Gale Ranch Middle School",
+		"address" => "6400 Main Branch Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-479-1500",
+		"url" => "http =>//www.grms.srvusd.net",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "The Goddard School",
+		"address" => "100 Gatekeeper Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-560-9694",
+		"url" => "goddardschool.com_",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Dougherty Valley High School",
+		"address" => "10550 Albion Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-479-6400",
+		"url" => "http =>//www.dvhigh.net",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Venture School",
+		"address" => "10540 Albion Rd",
+		"city" => "San Ramon",
+		"zip" => "945820",
+		"phone" => "925-479-1200",
+		"url" => "http =>//www.venture.srvusd.net",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Coyote Creek Elementary School",
+		"address" => "8700 Northgale Ridge Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-735-1183",
+		"url" => "http =>//www.ckes.srvusd.k12.ca.us",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Diablo Valley College",
+		"address" => "1690 Watermill Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-866-1822",
+		"url" => "http =>//www.srvc.net",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Gale Ranch YMCA Preschool",
+		"address" => "2200 Brookcliff Cir",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-648-1201",
+		"url" => "http =>//ymca-cba.org",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "California High School",
+		"address" => "9870 Boradmoor Dr",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-803-3200",
+		"url" => "calhigh.net_",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Neil Armstrong Elementary School",
+		"address" => "2849 Calais Dr",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-479-1600",
+		"url" => "http =>//www.naes.srvusd.k12.ca.us",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Pine Valley Middle School",
+		"address" => "3000 Pine Valley Rd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-479-7700",
+		"url" => "http =>//www.pvms.srvusd.k12.ca.us",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Golden View Elementary School",
+		"address" => "5025 Canyon Crest Dr",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-855-2700",
+		"url" => "http =>//www.goldenviewonline.com",
+		"cat_slug" => "schools",
+		"desc" => "Schools",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Creekside Park",
+		"address" => "1343 South Wedgewood Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-973-3290",
+		"url" => "govoutreach.com_",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "East Branch Park",
+		"address" => "Hartcourt Way",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "",
+		"url" => "",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Souyen Park",
+		"address" => "1548 Watermill Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "625-973-3290",
+		"url" => "san-ramon.ca.us_",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Bellingham Park",
+		"address" => "Bellingham Square",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "",
+		"url" => "",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Monarch Park",
+		"address" => "N Monarch Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-973-3290",
+		"url" => "http =>//www.sanramon.ca.gov/welcome.html",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Canyon Lakes Golf Course",
+		"address" => "640 Bollinger Canyon Way",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-735-6511",
+		"url" => "canyonlakesgolfclub.com_",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "San Ramon Sports",
+		"address" => "2411 Old Crow Canyon Rd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-831-9050",
+		"url" => "sanramonsports.com_",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "24 Hour Fitness",
+		"address" => "4450 Norris Canyon Rd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-244-9855",
+		"url" => "http =>//www.24hourfitness.com",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "San Ramon Aquatic Park",
+		"address" => "9900 Boradmoor Dr",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-973-3240",
+		"url" => "http =>//www.sanramon.ca.gov/Parks/programs/aquatics.htm/",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "San Ramon Central Park",
+		"address" => "12501 Alcosta Blvd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-973-3200",
+		"url" => "",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Bridges Golf Club",
+		"address" => "9000 S Gale Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-735-4253",
+		"url" => "http =>//www.thebridgesgolf.com",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Crow Canyon Country Club",
+		"address" => "711 Silver Lake Dr",
+		"city" => "Danville",
+		"zip" => "94526",
+		"phone" => "925-735-5700",
+		"url" => "crow-canyon.com_",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "San Ramon Golf Club",
+		"address" => "9430 Fincrest Ln",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-828-6100",
+		"url" => "golfsanramon.com_",
+		"cat_slug" => "recreation",
+		"desc" => "Recreation",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Crow Canyon Commons",
+		"address" => "3101 Crow Canyon Pl",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "",
+		"url" => "",
+		"cat_slug" => "shopping",
+		"desc" => "Shopping",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Target",
+		"address" => "2610 Bishop Dr",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-277-0202",
+		"url" => "http =>//www.target.com",
+		"cat_slug" => "shopping",
+		"desc" => "Shopping",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Orchard Supply Hardware",
+		"address" => "1041 Market Pl",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-275-1670",
+		"url" => "http =>//www.osh.com",
+		"cat_slug" => "shopping",
+		"desc" => "Shopping",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Staples",
+		"address" => "2710 Crow Canyon Rd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-552-1960",
+		"url" => "www.staples.com/",
+		"cat_slug" => "shopping",
+		"desc" => "Shopping",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Rite Aid",
+		"address" => "3207 Crow Canyon Pl",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-866-0505",
+		"url" => "http =>//www.Riteaid.com",
+		"cat_slug" => "shopping",
+		"desc" => "Shopping",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "CVS Pharmacy",
+		"address" => "490 Market Pl",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-327-0213",
+		"url" => "http =>//www.cvs.com",
+		"cat_slug" => "shopping",
+		"desc" => "Shopping",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "HomeGoods",
+		"address" => "120 Sunset Dr",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-277-1308",
+		"url" => "http =>//www.homegoods.com",
+		"cat_slug" => "shopping",
+		"desc" => "Shopping",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Blackhawk Plaza",
+		"address" => "3380 Blackhawk Plaza Cir",
+		"city" => "Danville",
+		"zip" => "94506",
+		"phone" => "925-736-2751",
+		"url" => "http =>//www.shopblackhawkplaza.com",
+		"cat_slug" => "shopping",
+		"desc" => "Shopping",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Safeway",
+		"address" => "11060 Bollinger Canyon Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-359-2001",
+		"url" => "safeway.com_",
+		"cat_slug" => "markets",
+		"desc" => "Supermarkets & Farmers Markets",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Lucky Food Center",
+		"address" => "3181 Crow Canyon Pl",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-866-1192",
+		"url" => "savemart.com_",
+		"cat_slug" => "markets",
+		"desc" => "Supermarkets & Farmers Markets",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Whole Foods Market",
+		"address" => "100 Sunset Dr",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-355-9000",
+		"url" => "http =>//wholefoodsmarket.com/",
+		"cat_slug" => "markets",
+		"desc" => "Supermarkets & Farmers Markets",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Windmill Farms Produce",
+		"address" => "2255 San Ramon Valley Blvd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-820-0747",
+		"url" => "http =>//www.windmillfarmsproduce.com",
+		"cat_slug" => "markets",
+		"desc" => "Supermarkets & Farmers Markets",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Nob Hill Foods",
+		"address" => "130 Market Pl",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-866-1022",
+		"url" => "http =>//www.raleys.com/www/home.jsp",
+		"cat_slug" => "markets",
+		"desc" => "Supermarkets & Farmers Markets",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Trader Joe's",
+		"address" => "85 Railroad Ave",
+		"city" => "Danville",
+		"zip" => "94526",
+		"phone" => "925-838-5757",
+		"url" => "http =>//www.traderjoes.com",
+		"cat_slug" => "markets",
+		"desc" => "Supermarkets & Farmers Markets",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Draeger's Market",
+		"address" => "4100 Blackhawk Plaza Cir",
+		"city" => "Danville",
+		"zip" => "94506",
+		"phone" => "925-648-5800",
+		"url" => "http =>//www.draegers.com",
+		"cat_slug" => "markets",
+		"desc" => "Supermarkets & Farmers Markets",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "King Palace",
+		"address" => "11040 Bollinger Canyon Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-964-1328",
+		"url" => "kingpalacecuisine.com_",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Ascona Pizza Co",
+		"address" => "11020 Bollinger Canyon Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-736-0606",
+		"url" => "asconapizza.com_",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Peet's Coffee & Tea",
+		"address" => "11000 Bollinger Canyon Rd",
+		"city" => "San Ramon",
+		"zip" => "94582",
+		"phone" => "925-964-9195",
+		"url" => "peets.com_",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Wingstop",
+		"address" => "3171 Crow Canyon Pl",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-867-9464",
+		"url" => "wingstop.com_",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Primavera Ristorante & Bar",
+		"address" => "3124 Crow Canyon Pl",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-830-9500",
+		"url" => "pastaprimavera.us_",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Zachary's Chicago Pizza",
+		"address" => "3110 Crow Canyon Pl",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-244-1222",
+		"url" => "zacharys.com_",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Fat Maddie's Grille",
+		"address" => "2005 Crow Canyron Pl",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-355-1975",
+		"url" => "http =>//www.fatmaddiesgrille.com",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "In-N-Out",
+		"address" => "2270 San Ramon Valley Blvd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "800-786-1000",
+		"url" => "http =>//www.in-n-out.com",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Fresh Millions Restaurant",
+		"address" => "2550 San Ramon Valley Blvd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-838-3200",
+		"url" => "http =>//www.freshmillions.com",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Chipotle Mexican Grill",
+		"address" => "2421 San Ramon Valley Blvd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-820-4110",
+		"url" => "http =>//www.chipotle.com",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Mexi's Taqueria",
+		"address" => "9110 Alcosta Blvd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-803-1111",
+		"url" => "mexxis.com_",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Blackhawk Grill",
+		"address" => "3540 Blackhawk Plaza Cir",
+		"city" => "Danville",
+		"zip" => "94506",
+		"phone" => "925-736-4295",
+		"url" => "www.blackhawkgrille.com/",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Blue Gingko",
+		"address" => "3496 Blackhawk Plaza Cir",
+		"city" => "Danville",
+		"zip" => "94506",
+		"phone" => "925-648-7838",
+		"url" => "bluegingkosushi.com/",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "Prickly Pear Cantina",
+		"address" => "3421 Blackhawk Plaza Cir",
+		"city" => "Danville",
+		"zip" => "94506",
+		"phone" => "925-984-2363",
+		"url" => "www.thepricklypearcantina.com/",
+		"cat_slug" => "dining",
+		"desc" => "Dining",
+		"lat" => 0.0,
+		"lng" => 0.0
+	),
+	array( 
+		"title" => "San Ramon Regional Medical Center",
+		"address" => "6001 Norris Canyon Rd",
+		"city" => "San Ramon",
+		"zip" => "94583",
+		"phone" => "925-275-9200",
+		"url" => "oursanramonhospital.com_",
+		"cat_slug" => "other",
+		"desc" => "Other",
+		"lat" => 0.0,
+		"lng" => 0.0
+	)
+);
+
+foreach ($poi as $key=>$value) {
+	$address = urlencode($value["address"].', '.$value['city'].', '.$value['state'].' '.$value['zip']);
+	$url = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=false';
+	$url .= '&address='.$address;
+
+	$response = wp_remote_get( $url );
+	if( is_wp_error( $response ) ) {
+	   $error_message = $response->get_error_message();
+	   echo "Something went wrong: $error_message";
+	}
+
+	$response2 = json_decode($response['body']);
+    $location = $response2->results[0]->geometry->location;
+    $poi[$key]['lat'] = $location->lat;
+    $poi[$key]['lng'] = $location->lng;
+
+}
+echo json_encode($poi);
+
+get_footer();
+
+?>
+
+
+
