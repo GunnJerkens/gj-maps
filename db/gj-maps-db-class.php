f1d29e52a23a1732b3db9799abaa5c6ac2b90b00
patrickshampine <patrick@patrickshampine.com>
2014-05-30 00:16:10 -0700

    Audited and adjusted all db class methods

--- db/gj-maps-db-class.php
+++ db/gj-maps-db-class.php
@@ -14,9 +14,9 @@ class gjMapsDB {
 
   function deleteAllData() {
 
+    $response['poi'] = $this->deleteAllPOI();
     $response['cat'] = $this->deleteAllCat();
     $response['maps'] = $this->deleteAllMaps();
-    $response['poi'] = $this->deleteAllPOI();
 
     return $response;
 
@@ -55,6 +55,46 @@ class gjMapsDB {
   * Map Database Functions
   */
 
+  function get_map($type='OBJECT', $where='1=1') {
+    global $wpdb;
+
+    $table_name = $this->mapsTable();
+
+    $query = $this->wpdb->get_results(
+      "
+      SELECT *
+      FROM $table_name
+      WHERE $where
+      ",
+      $type
+    );
+
+    return $query;
+
+  }
+
+  /*
+  * I don't know what this does?
+  * It should probably move to the admin-class, it's not a db function?
+  */
+  function get_map_key($id, $obj) {
+
+    $mapKey = false;
+
+    foreach ($obj as $key => $value) {
+
+      if ($value->id == $id) {
+
+        $mapKey = $key;
+
+      }
+
+    }
+
+    return $mapKey;
+
+  }
+
   function maxMapID() {
 
     $table_name = $this->mapsTable();
@@ -92,7 +132,7 @@ class gjMapsDB {
 
     $table_name = $this->mapsTable();
 
-    $rows_affected = $this->wpdb->insert(
+    $insert = $this->wpdb->insert(
       $table_name,
       array(
         'id'=>$id,
@@ -100,7 +140,7 @@ class gjMapsDB {
       )
     );
 
-    return $rows_affected;
+    return $insert;
 
   }
 
@@ -108,7 +148,7 @@ class gjMapsDB {
 
     $table_name = $this->mapsTable();
 
-    $rows_affected = $wpdb->update(
+    $insert = $this->wpdb->update(
       $table_name, 
       array(
         'name'=>$ms['name'],
@@ -119,17 +159,19 @@ class gjMapsDB {
       array('id'=>$ms['id'])
     );
 
+    return $insert;
+
   }
 
   function deleteAllMaps() {
 
     $table_name = $this->mapsTable();
 
-    $response = $this->wpdb->query(
+    $query = $this->wpdb->query(
       "TRUNCATE TABLE $table_name"
     );
 
-    return $response;
+    return $query;
 
   }
 
@@ -137,36 +179,58 @@ class gjMapsDB {
   * POI Database Functions
   */
 
+  function get_poi($type='OBJECT', $where='1=1') {
+
+    $table_name = $this->poiTable();
+
+    $query = $this->wpdb->get_results(
+      "
+      SELECT *
+      FROM $table_name
+      WHERE $where
+      ",
+      $type
+    );
+
+    return $query;
+
+  }
+
   function savePOI ($poi) {
 
-    global $wpdb;
-    $table_name = $wpdb->prefix . "gjm_poi";
+    $table_name = $this->poiTable();
 
     foreach ($poi as $key=>$value) {
-      $rows_affected = $wpdb->insert( $table_name, array(
-        'cat_id'=>$value['cat_id'],
-        'map_id'=>$value['map_id'],
-        'name'=>$value['name'],
-        'address'=>$value['address'],
-        'city'=>$value['city'],
-        'state'=>$value['state'],
-        'zip'=>$value['zip'],
-        'country'=>$value['country'],
-        'phone'=>$value['phone'],
-        'url'=>$value['url'],
-        'lat'=>$value['lat'],
-        'lng'=>$value['lng']
-      ) );
+
+      $insert[] = $this->wpdb->insert(
+        $table_name, 
+        array(
+          'cat_id'=>$value['cat_id'],
+          'map_id'=>$value['map_id'],
+          'name'=>$value['name'],
+          'address'=>$value['address'],
+          'city'=>$value['city'],
+          'state'=>$value['state'],
+          'zip'=>$value['zip'],
+          'country'=>$value['country'],
+          'phone'=>$value['phone'],
+          'url'=>$value['url'],
+          'lat'=>$value['lat'],
+          'lng'=>$value['lng']
+        )
+      );
+
     }
+
+    return $insert;
 
   }
 
   function editPOI ($poi) {
 
-    global $wpdb;
-    $table_name = $wpdb->prefix . "gjm_poi";
+    $table_name = $this->poiTable();
 
-    $rows_affected = $wpdb->update(
+    $update = $this->wpdb->update(
       $table_name,
       array(
         'cat_id'=>$poi['cat_id'],
@@ -183,16 +247,19 @@ class gjMapsDB {
       ),
       array( 'id'=>$poi['id'] )
     );
+
+    return $update;
+
   }
 
-  function deletePOI ($id = false) {
-    global $wpdb;
+  function deletePOI($id = false) {
 
-    $table_name = $wpdb->prefix . "gjm_poi";
+    $table_name = $this->poiTable();
 
-    if ($id) {
-      $wpdb->query(
-          $wpdb->prepare(
+    if($id) {
+
+      $query = $this->wpdb->query(
+          $this->wpdb->prepare(
             "
             DELETE FROM $table_name 
             WHERE id = %d
@@ -200,22 +267,22 @@ class gjMapsDB {
             $id
         )
       );
-    } else {
-      $wpdb->query(
-          "TRUNCATE TABLE $table_name"
-       );
-     }
+
+    }
+
+    return $query;
+
   }
 
   function deleteAllPOI() {
 
     $table_name = $this->poiTable();
 
-    $response = $this->wpdb->query(
+    $query = $this->wpdb->query(
       "TRUNCATE TABLE $table_name"
     );
 
-    return $response;
+    return $query;
 
   }
 
@@ -223,34 +290,68 @@ class gjMapsDB {
   * Category Database Functions
   */
 
-  function saveCat ($cat) {
+  function get_cat($type='OBJECT', $where='1=1') {
 
-    global $wpdb;
-    $table_name = $wpdb->prefix . "gjm_cat";
-    $rows_affected = $wpdb->insert( $table_name, array( 'name'=>$cat['name'], 'color'=>$cat['color'], 'icon'=>$cat['icon'], ) );
+    $table_name = $this->catTable();
+  
+    $query = $this->wpdb->get_results(
+      "
+      SELECT *
+      FROM $table_name
+      WHERE $where
+      ",
+      $type
+    );
+
+    return $query;
+  
+  }
+
+  function saveCat($cat) {
+
+    $table_name = $this->catTable();
+
+    $insert = $this->wpdb->insert(
+      $table_name, 
+      array(
+        'name'=>$cat['name'],
+        'color'=>$cat['color'],
+        'icon'=>$cat['icon'],
+      )
+    );
+
+    return $insert;
 
   }
 
-  function editCat ($cat) {
-    if(array_key_exists('icon',$cat) && $cat['icon'] == null) {
-      unset($cat['icon']);
-    }
-    global $wpdb;
-    $table_name = $wpdb->prefix . "gjm_cat";
+  function editCat($cat) {
 
-    $rows_affected = $wpdb->update( 
-      $table_name, $cat, array( 'id'=>$cat['id']) 
+    if(array_key_exists('icon',$cat) && $cat['icon'] == null) {
+
+      unset($cat['icon']);
+
+    }
+
+    $table_name = $this->catTable();
+
+    $update = $this->wpdb->update( 
+      $table_name, 
+      $cat, 
+      array(
+        'id'=>$cat['id']
+      ) 
     );
+
+    return $update;
 
   }
 
   function deleteCat ($id) {
-    global $wpdb;
 
-    $table_name = $wpdb->prefix . "gjm_cat";
+    $table_name = $this->catTable();
 
-    $wpdb->query(
-      $wpdb->prepare(
+    $query = $this->wpdb->query(
+      $this->wpdb->prepare(
         "
         DELETE FROM $table_name 
         WHERE id = %d
@@ -259,82 +360,20 @@ class gjMapsDB {
       )
     );
 
+    return $query;
+
   }
 
   function deleteAllCat() {
 
     $table_name = $this->catTable();
 
-    $response = $this->wpdb->query(
+    $query = $this->wpdb->query(
       "TRUNCATE TABLE $table_name"
     );
 
-    return $response;
-
-  }
-
-  /*
-  * Retrieval Database Functions
-  */
-
-  function get_poi($type='OBJECT', $where='1=1') {
-    //Allows you to set the type of the return value (assc. array or stdClass) and the WHERE clause, if necessary
-    global $wpdb;
-
-    $table_name = $wpdb->prefix . "gjm_poi";
-    $query = $wpdb->get_results(
-      "
-      SELECT *
-      FROM $table_name
-      WHERE $where
-      ",
-      $type
-    );
-
     return $query;
+
   }
-
-  function get_cat($type='OBJECT', $where='1=1') {
-    //Allows you to set the type of the return value (assc. array or stdClass) and the WHERE clause, if necessary
-    global $wpdb;
-
-    $table_name = $wpdb->prefix . "gjm_cat";
-    $query = $wpdb->get_results(
-      "
-      SELECT *
-      FROM $table_name
-      WHERE $where
-      ",
-      $type
-    );
-
-    return $query;
-  }
-
-  function get_map($type='OBJECT', $where='1=1') {
-    //Allows you to set the type of the return value (assc. array or stdClass) and the WHERE clause, if necessary
-    global $wpdb;
-
-    $table_name = $wpdb->prefix . "gjm_maps";
-    $query = $wpdb->get_results(
-      "
-      SELECT *
-      FROM $table_name
-      WHERE $where
-      ",
-      $type
-    );
-
-    return $query;
-  }
-
-  function get_map_key($id, $obj) {
-    foreach ($obj as $key => $value) {
-      if ($value->id == $id) {
-        return $key;
-      }
-    }
-  }
-
 
 }
